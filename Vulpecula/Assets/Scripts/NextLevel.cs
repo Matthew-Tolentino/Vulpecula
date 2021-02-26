7fdb4b6d21df1f788b3d732f871b4afb73327181
Matthew <lemtolen@ucsc.edu>
2021-02-26 13:59:03 -0800

    Added cerma control and movement to vulpecula in 1st level

---
+++ Vulpecula/Assets/Scripts/NextLevel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevel : MonoBehaviour
+{
+    void OnTriggerEnter(Collider obj)
+    {
+        if (obj.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
