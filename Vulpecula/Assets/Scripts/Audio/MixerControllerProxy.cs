145085690229148ff1f694edf3125c99418204e4
Niccolo Abate <lofieloomis@gmail.com>
2021-04-07 20:48:22 -0800

    Fixed setting bug with proxy, some FMOD spirit world stuff
    Fixed setting bug with proxy, some FMOD spirit world stuff

---
+++ Vulpecula/Assets/Scripts/Audio/MixerControllerProxy.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MixerControllerProxy : MonoBehaviour
+{
+    public void SetMasterVolume(float volume)
+    {
+        MixerController.instance.SetMasterVolume(volume);
+    }
+}
