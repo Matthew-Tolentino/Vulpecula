cb83e211bcf8349db7c7dccc787aee3aad6ee1f1
Matthew <lemtolen@ucsc.edu>
2021-05-09 20:42:32 -0700

    added minimap icons to spirts, sped up turning of vulpecula, added spirit portraits

---
+++ Vulpecula/Assets/Scripts/UI/Minimap/SpiritMapIcon.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiritMapIcon : MonoBehaviour
+{
+    [SerializeField] Transform spiritTarget;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(spiritTarget.position.x, transform.position.y, spiritTarget.position.z);
+    }
+}
