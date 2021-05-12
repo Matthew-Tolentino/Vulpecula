6cf999785401623fd172424523f6e9c6f1e5e9c7
Matthew <lemtolen@ucsc.edu>
2021-05-12 11:41:44 -0700

    Added teleportation to start of lake puzzle

---
+++ Vulpecula/Assets/Scripts/WorldPuzzles/Lake_SpiritWorld/RestartLakePuzzle.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class RestartLakePuzzle : MonoBehaviour
+{
+    Transform player;
+    Transform startPos;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        startPos = GameObject.FindGameObjectWithTag("Lake_Puzzle_Start").transform;
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Player"))
+        {
+            RestartPuzzle();
+        }
+    }
+
+    private void RestartPuzzle()
+    {
+        print("Restarting puzzle");
+        player.position = startPos.position;
+    }
+}
