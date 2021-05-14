eda87f75efc43590a24a0713fd479ca4975201ec
Niccolo Abate <lofieloomis@gmail.com>
2021-05-13 16:44:57 -0800

    Fixed footsteps for new animations, new level design. Added box sounds. Updated music
    Fixed footsteps for new animations, new level design. Added box sounds. Updated music. (Violin part needs a bit of editing still to make it sound better).
    
    Added a bunch of puzzle triggers and other things. Still have a few more things I want to do to finish all of this, but can't right now.

---
+++ Vulpecula/Assets/Scripts/Audio/BoxSounds.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxSounds : MonoBehaviour
+{
+    Rigidbody rb;
+
+    [SerializeField]
+    private float horizontalVelocityThreshold = 5;
+
+    private FMOD.Studio.EventInstance moveSoundsInstance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 horizontalVector = new Vector3(1, 0, 1);
+        Vector3 horizontalVelocity = rb.velocity;
+        horizontalVelocity.Scale(horizontalVector);
+
+        if (horizontalVelocity.sqrMagnitude >= horizontalVelocityThreshold)
+        {
+            PlayMoveSound();
+        }
+    }
+
+    private void PlayMoveSound()
+    {
+        if (moveSoundsInstance.isValid())
+        {
+            moveSoundsInstance.getPlaybackState(out var state);
+            if (state == FMOD.Studio.PLAYBACK_STATE.PLAYING)
+                return;
+        }
+
+        moveSoundsInstance = FMODUnity.RuntimeManager.CreateInstance("event:/SFX/Box Dragging");
+
+        moveSoundsInstance.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(gameObject));
+        moveSoundsInstance.start();
+        moveSoundsInstance.release();
+    }
+}
