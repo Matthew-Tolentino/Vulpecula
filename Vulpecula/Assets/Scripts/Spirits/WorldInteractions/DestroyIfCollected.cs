0667e567d84a6559af5d91f9709515f115feeef2
Trystan Nguyen <tryncalibur@gmail.com>
2021-06-01 20:03:45 -0700

    FinalTouches_TSY

---
+++ Vulpecula/Assets/Scripts/Spirits/WorldInteractions/DestroyIfCollected.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyIfCollected : MonoBehaviour
+{
+    public SpiritMovement_Floating target;
+    public GameObject dialogue;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target.state != "Spawn") 
+        {
+            Destroy(dialogue);
+            Destroy(gameObject);
+        }
+    }
+}
